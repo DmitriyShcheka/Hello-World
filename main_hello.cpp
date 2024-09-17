1d2292d2f4e96feb119490e2894e53b06c0beb68
Egor-Shast-rezka <st129457@student.spbu.ru>
2024-09-17 22:41:11 +0300

    added connection with test_data.txt

--- main_hello.cpp
+++ main_hello.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
+#include <fstream>
 #include "main.h"
 
 using namespace std;
 
 void print_hello() {
-	cout << "Hello, world!" << endl;
-	while (true){
-		string str;
-		cout << "Write your text: ";
-		cin >> str;
-		cout << "Hello, " << str << "!" << endl;
+	ofstream file("test_data.txt", std::ios::app);
+
+	if (file.is_open()){
+		cout << "Hello, world!" << endl;
+
+		file << "Program was started.\n\n";
+		file << "print_hello was called.\n";
+
+		while (true){
+			string str;
+			cout << "Write your text: ";
+			cin >> str;
+			cout << "Hello, " << str << "!" << endl;
+
+			file << "user write text:" << str << "\n";
+			file << "program completed successfully.\ncucle update.\n";
+		}
+		file.close();
+	} else {
+		cerr << "unable to open file test_data.txt" << endl;
 	}
 }
