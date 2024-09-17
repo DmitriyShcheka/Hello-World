71313ed157b7a6f70c7ea27cd0f432bac95c3212
Egor-Shast-rezka <st129457@student.spbu.ru>
2024-09-17 22:06:03 +0300

    created new main file

--- main.cpp
+++ main.cpp
@@ -1,14 +1,5 @@
-#include <iostream>
 #include "main.h"
 
-using namespace std;
-
-void print_hello() {
-	cout << "Hello, world!" << endl;
-	while (true){
-		string str;
-		cout << "Write your text: ";
-		cin >> str;
-		cout << "Hello, " << str << "!" << endl;
-	}
+int main(){
+	print_hello();
 }
